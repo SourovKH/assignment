5a60e9768d4999855c20f531480784b3a9f3e22f
Sourov <SourovKH@users.noreply.github.com>
2023-04-12 12:50:21 +0530

    even number checking is fixed

---
+++ src/arrays.js
@@ -0,0 +1,23 @@
+const math = require("../src/math.js");
+
+const selectEvenNumbers = function(numbers) {
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (math.isEven(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
+}
+
+const selectOddNumbers = function(numbers) {
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (math.isOdd(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
+}
+
+exports.selectEvenNumbers = selectEvenNumbers;
